a10348bd6198d46479223290a067dc90b473fd57
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-19 19:19:15 +0000

    Fixed task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
